b00d401c06df3a83f4fffe6fb85f363890322983
do2bfree <alexey.tivonenko@gmail.com>
2020-12-13 15:09:03 +0300

    Add mail submit func

--- mail.php
+++ mail.php
@@ -1,15 +1,11 @@
 <?php
 
 $recepient = "alexey.tivonenko@gmail.com";
-$sitename = "Zizoo board";
+$sitename = "Paco puzzles";
 
-$fio = trim($_POST["fio"]);
 $email = trim($_POST["email"]);
-$tel = trim($_POST["tel"]);
-$date = trim($_POST["date"]);
-$time = trim($_POST["time"]);
-$notes = trim($_POST["notes"]);
-$message = "Имя: $fio \nПочта: $email \nТелефон: $tel \nДата: $date \nВремя: $time \nТекст: $notes";
 
-$pagetitle = "Новая заявка с сайта \"$sitename\"";
+$message = "Почта: $email";
+$pagetitle = "Новый имейл с сайта \"$sitename\"";
+
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
