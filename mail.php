a79fc63eaa45030fc0a1e7c7798e0b1d66d53294
do2bfree <alexey.tivonenko@gmail.com>
2020-12-13 22:30:16 +0300

    Test email form

--- mail.php
+++ mail.php
@@ -3,13 +3,10 @@
 $recepient = "alexey.tivonenko@gmail.com";
 $sitename = "Zizoo board";
 
-$fio = trim($_POST["fio"]);
+
 $email = trim($_POST["email"]);
-$tel = trim($_POST["tel"]);
-$date = trim($_POST["date"]);
-$time = trim($_POST["time"]);
-$notes = trim($_POST["notes"]);
-$message = "Имя: $fio \nПочта: $email \nТелефон: $tel \nДата: $date \nВремя: $time \nТекст: $notes";
+
+$message = "Почта: $email";
 
 $pagetitle = "Новая заявка с сайта \"$sitename\"";
 mail($recepient, $pagetitle, $message, "Content-type: text/plain; charset=\"utf-8\"\n From: $recepient");
